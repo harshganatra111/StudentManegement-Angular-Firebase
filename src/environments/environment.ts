db001061a41e06ca47f460b50e9ad050ad3704e4
harshganatra111 <33564627+harshganatra111@users.noreply.github.com>
2020-10-30 12:15:56 +0530

    add bootstrap and firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,21 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+        apiKey: "AIzaSyDVEFmqJoZCbMU6rulqIqo6K4auKBrZwRQ",
+        authDomain: "studentdb-a961d.firebaseapp.com",
+        databaseURL: "https://studentdb-a961d.firebaseio.com",
+        projectId: "studentdb-a961d",
+        storageBucket: "studentdb-a961d.appspot.com",
+        messagingSenderId: "664228425966",
+        appId: "1:664228425966:web:f8acb0daf4806fd58583ff",
+        measurementId: "G-K9BHLQC6BH"
+  }
+    
+
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
